7b126990d1f72340ea0871b42380c0bd7e9ba2ba
OSCAR EMILIO MARTINEZ MARTINEZ <154422236+EMILIO2802M@users.noreply.github.com>
2025-03-28 10:43:53 -0600

    Add files via upload

---
+++ cadena con len.py
@@ -0,0 +1,5 @@
+cadena=str(input("Dime tu nombre--->"))
+r=len(cadena)
+print ("El resultado es: ", r)
+
+#len. Permite leer la longitud de una cadena, es decir, el numero de caracteres que forma la cadena.
