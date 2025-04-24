128598a78f4bd1a08f74d3d427109a6ee4657c12
OSCAR EMILIO MARTINEZ MARTINEZ <154422236+EMILIO2802M@users.noreply.github.com>
2025-04-23 22:53:32 -0600

    Add files via upload

---
+++ diccionario.py
@@ -0,0 +1,28 @@
+def suma():
+    x = int(input("Dame un numero: "))
+    r = int(input("Dme otro numero: "))
+    z = x+r
+    print ("El resultado es--->", z)
+    
+def resta():
+    x = int(input("Dame un numero: "))
+    r = int(input("Dme otro numero: "))
+    z = x-r
+    print ("El resultado es--->", z)
+    
+def multiplicacion():
+    x = int(input("Dame un numero: "))
+    r = int(input("Dme otro numero: "))
+    z = x*r
+    print ("El resultado es--->", z)
+    
+def division():
+    x = int(input("Dame un numero: "))
+    r = int(input("Dme otro numero: "))
+    z = x/r
+    print ("El resultado es--->", z)
+    
+suma()
+resta()
+multiplicacion()
+division()
